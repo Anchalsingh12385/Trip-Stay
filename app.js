206b9712dfc7bdfcc27556f2f8ba64525a56924d
Anchalsingh12385 <anchalkumarissingh@gmail.com>
2026-02-18 17:49:54 +0530

    updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const   Listing = require("./models/listing.js");
 const path = require("path");
-
+const methodOverride = require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -21,7 +21,8 @@ await mongoose.connect(MONGO_URL);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/",(req,res) =>{
     res.send("i am robot");
@@ -33,10 +34,44 @@ app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index", {  allListings });
 });
-//
+///NEW ROUTE
+app.get("/listings/new",(req,res) =>{
+    res.render("listings/new.ejs");
+})
 
+// SHOW ROUTE
 
+app.get("/listings/:id",async (req,res) =>{
+    let {id} = req.params;
+  const listing = await  Listing.findById(id);
+  res.render("listings/show.ejs",{listing});
+})
 
+//CREATE ROUTE
+app.post("/listings",async(req,res)=>{
+  const newListing=  new Listing (req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+//EDIT ROUTE
+app.get("/listings/:id/edit", async (req,res) =>{
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+});
+//UPDATE ROUTE
+app.put("/listings/:id", async (req,res) =>{
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing});
+   res. redirect(`/listings/${id}`);
+})
+//DELETE ROUTE
+app.delete("/listings/:id", async (req,res) =>{
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+})
 
 
 //TEST ROUTE - Visit http://localhost:8080/testListing once to add sample data
