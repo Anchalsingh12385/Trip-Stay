87d657953a9ec7ef1f88248c8f051ede54b7fcf0
Anchalsingh12385 <anchalkumarissingh@gmail.com>
2026-03-01 18:43:40 +0530

    updated

--- classroom/server.js
+++ classroom/server.js
@@ -2,49 +2,39 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
+const cookieParser = require("cookie-parser");
 
+app.use(cookieParser("secretcode"));
+
+app.get("/singnedcookie",(req,res)=>{
+    res.cookie("made-in", "India",{ signed: true});
+    res.send("signed cookie sent");
+});
+
+app.get("/verify",(req,res)=>{
+    console.log(req.signedCookies);
+    res.send("verified");
+})
+
+
+app.get("/getcookies",(req,res)=>{
+    res.cookie("greet","namaste");
+    res.cookie("madeIn","India");
+    res.send("sent you some cookies!");
+});
+
+app.get("/greet",(req,res)=>{
+    let{ name = "anonymous"} = req.cookies;
+    res.send(`Hi,${name}`);
+});
 
 app.get("/",(req,res)=>{
+    console.dir(req.cookies);
     res.send("Hi,I am root!");
 });
 
 app.use("/users",users);
 app.use("/posts",posts);
-
-//Index -users
-app.get("/users",(req,res)=>{
-    res.send("GET for users");
-});
-//Show -users
-app.get("/users/:id",(req,res)=>{
-    res.send("GET for userid");
-});
-//post -users
-app.post("/users",(req,res)=>{
-    res.send("POST for users");
-});
-//Delete -users
-app.delete("/users/:id",(req,res)=>{
-    res.send("DELETE for user id");
-});
-
-//Index -post
-app.get("/posts",(req,res)=>{
-    res.send("GET for posts");
-});
-//Show -post
-app.get("/posts/:id",(req,res)=>{
-    res.send("GET for postid");
-});
-//post -posts
-app.post("/posts",(req,res)=>{
-    res.send("POST for posts");
-});
-//Delete - posts
-app.delete("/posts/:id",(req,res)=>{
-    res.send("DELETE for post id");
-});
-
 
 
 
