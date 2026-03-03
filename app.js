d34e23d261376fdb84c071936386f77808fcb9f9
Anchalsingh12385 <anchalkumarissingh@gmail.com>
2026-03-03 22:04:39 +0530

    updated

--- app.js
+++ app.js
@@ -69,14 +69,14 @@ res.locals.error = req.flash("error");
 next();
 });
 
-app.get("/demouser",async(req,res)=>{
-    let fakeUser = new User({
-        email: "student@gmail.com",
-        username:"delta-student",
-    });
-    let registeredUser = await User.register(fakeUser,"helloworld");
-    res.send(registeredUser);
-});
+//app.get("/demouser",async(req,res)=>{
+  //  let fakeUser = new User({
+    //    email: "student@gmail.com",
+      //  username:"delta-student",
+    //});
+    //let registeredUser = await User.register(fakeUser,"helloworld");
+    //res.send(registeredUser);
+//});
 
 
 
