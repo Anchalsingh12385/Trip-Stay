4ac898b78a6453142f20e3c9410533d7148681f9
Anchalsingh12385 <anchalkumarissingh@gmail.com>
2026-03-09 16:27:34 +0530

    updated

--- routes/listing.js
+++ routes/listing.js
@@ -3,14 +3,17 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const   Listing = require("../models/listing.js");
 const {isLoggedIn,isOwner,validateListing} = require("../middlewares.js");
-
 const listingController = require("../controllers/listing.js");
-
+const multer = require("multer");
+const upload = multer({ dest: 'uploads/'})
 router.route("/")
    .get(wrapAsync(listingController.index))
 
-   .post(isLoggedIn, validateListing,wrapAsync(listingController.createListing));
-
+  // .post(isLoggedIn, validateListing,wrapAsync(listingController.createListing));
+ .post( upload.single('listing[image]'),(req,res)=>{
+    res.send(req.file);
+ });
+   
    ///NEW ROUTE
 router.get("/new",isLoggedIn, listingController.renderNewForm);
 
